29d75a60c4cd244ff0056d6a256afc683014de3f
kabakae <kabakaevans@gmail.com>
2024-07-25 19:38:26 +0000

    00

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,10 @@
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle of height n.
-    
+
     Args:
         n (int): The number of rows in Pascal's Triangle.
-        
+
     Returns:
         list of lists: Pascal's Triangle.
     """
@@ -16,13 +16,12 @@ def pascal_triangle(n):
     for i in range(n):
         # Create a new row with `i + 1` elements initialized to 1
         row = [1] * (i + 1)
-        
+
         # Compute the values for non-edge elements
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        
+
         # Append the row to the triangle
         triangle.append(row)
 
     return triangle
-
