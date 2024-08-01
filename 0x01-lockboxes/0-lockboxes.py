dcdfb3cc9458e34e4760cd2d7247d093966990cf
kabakae <kabakaevans@gmail.com>
2024-08-01 07:06:08 +0000

    00

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,12 @@
 def canUnlockAll(boxes):
     # Create a set to store keys we have
     keys = set()
-    # The first box is already unlocked, so we start with the key to the first box
+    # The first box is already unlocked, so we start with the key to the firs
     keys.add(0)
-    
+
     # Use a list to keep track of boxes we need to check
     to_check = [0]
-    
+
     while to_check:
         current_box = to_check.pop()
         for key in boxes[current_box]:
@@ -14,6 +14,6 @@ def canUnlockAll(boxes):
                 keys.add(key)
                 if key < len(boxes):
                     to_check.append(key)
-    
+
     # If we have keys to all boxes, we should be able to unlock all boxes
     return len(keys) == len(boxes)
