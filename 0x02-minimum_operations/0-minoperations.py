77c8ac3fdb0900ff8516c415e982808f38f2dccf
root <kabakaevans@gmail.com>
2024-08-11 09:15:16 +0000

    00

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,28 @@
 def minOperations(n):
+    """
+    Calculate the minimum number of operations needed to reach exactly n 'H' characters
+    using only the Copy All and Paste operations.
+
+    Args:
+        n (int): The target number of 'H' characters.
+
+    Returns:
+        int: The minimum number of operations needed to achieve exactly n 'H' characters.
+             Returns 0 if it is impossible to achieve exactly n characters.
+    """
     if n <= 1:
         return 0  # If n is 1 or less, it's either not possible or trivial
 
     def get_factors(x):
+        """
+        Get all factors of a given number x.
+
+        Args:
+            x (int): The number to factorize.
+
+        Returns:
+            list: A sorted list of factors of x.
+        """
         factors = []
         for i in range(2, int(x**0.5) + 1):
             if x % i == 0:
@@ -14,12 +34,7 @@ def minOperations(n):
     
     operations = float('inf')
 
-    # For each factor of n, calculate the number of operations
     for factor in get_factors(n):
-        # number of operations needed for this factor is:
-        # (factor - 1) pastes + 1 copy all operation
-        # Plus the operations to reach 'factor'
-        # i.e., we need factor operations to build 'factor' H's
         ops = (n // factor) + (factor - 1)
         operations = min(operations, ops)
     
